5e9955e85d20fa3caa166fc5d6c9055282a720c0
Jesse Pendergrass <compunautics@gmail.com>
2019-05-20 17:38:45 -0500

    Adding more output so that it's easier to know where we are in the process

--- bootstrap/05_compunaut_init_salt.sh
+++ bootstrap/05_compunaut_init_salt.sh
@@ -4,19 +4,29 @@ cd "${0%/*}"
 source ./compunaut_functions
 
 ### HYPERVISOR SETUP
+  echo_red "SET UP HYPERVISORS"
+  echo_blue "Install KVM, boot, and salt VMs"
   time salt-run state.orch orch.create_and_salt_vms --state-output=mixed
 
 ### DEPLOY COMPUNAUT
   minion_wait
+  echo_red "SET UP DEFAULT ENVIRONMENT"
   time salt-run state.orch orch.update_data --state-output=mixed # update data
+  echo_blue "Generate and deploy PKI"
   time salt-run state.orch orch.generate_pki --state-output=mixed
+  echo_blue "Install default environment, and apply iptables rules"
   time salt-run state.orch orch.apply_default_env --state-output=mixed
   minion_wait
+  echo_red "DEPLOY COMPUNAUT"
+  echo_blue "Install Keepalived, DNS, and Consul"
   time salt-run state.orch orch.install_keepalived_dns_consul --state-output=mixed
   time salt-run state.orch orch.update_data --state-output=mixed # update data
+  echo_blue "Install MySQL and InfluxDB"
   time salt-run state.orch orch.install_dbs --state-output=mixed
+  echo_blue "Install OpenLDAP"
   time salt-run state.orch orch.install_openldap --state-output=mixed
   minion_wait
+  echo_blue "Install Compunaut Applications"
   time salt-run state.orch orch.install_apps --state-output=mixed
 
   echo_green "Waiting 120 seconds"
