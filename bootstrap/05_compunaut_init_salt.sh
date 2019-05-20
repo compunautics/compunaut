1c044c0c0d572e751df7fa292079b265dac99d2e
Jesse Pendergrass <compunautics@gmail.com>
2019-05-20 18:06:49 -0500

    Further modifications to take out unneeded waits and introduce better formats

--- bootstrap/05_compunaut_init_salt.sh
+++ bootstrap/05_compunaut_init_salt.sh
@@ -10,13 +10,16 @@ source ./compunaut_functions
 
 ### DEPLOY COMPUNAUT
   minion_wait
+
   echo_red "SET UP DEFAULT ENVIRONMENT"
   time salt-run state.orch orch.update_data --state-output=mixed # update data
   echo_blue "Generate and deploy PKI"
   time salt-run state.orch orch.generate_pki --state-output=mixed
   echo_blue "Install default environment, and apply iptables rules"
   time salt-run state.orch orch.apply_default_env --state-output=mixed
+
   minion_wait
+
   echo_red "DEPLOY COMPUNAUT"
   echo_blue "Install Keepalived, DNS, and Consul"
   time salt-run state.orch orch.install_keepalived_dns_consul --state-output=mixed
@@ -25,17 +28,17 @@ source ./compunaut_functions
   time salt-run state.orch orch.install_dbs --state-output=mixed
   echo_blue "Install OpenLDAP"
   time salt-run state.orch orch.install_openldap --state-output=mixed
+
   minion_wait
+
   echo_blue "Install Compunaut Applications"
   time salt-run state.orch orch.install_apps --state-output=mixed
 
-  echo_green "Waiting 120 seconds"
-  sleep 120
-
 # FINAL SETUP
-  time salt-run state.orch orch.update_data --state-output=mixed # update data
+  minion_wait
 
   echo_red "FINAL SETUP"
+  time salt-run state.orch orch.update_data --state-output=mixed # update data
   time salt-run state.orch orch.highstate --state-output=mixed
 
   time ./compunaut_ssh_keys_update.sh
