945e02c444afd04d32db9262c8ad5b9f8532e4df
Jesse Pendergrass <compunautics@gmail.com>
2019-06-06 13:14:39 -0500

    Shutdown the rundeck nodes first

--- bootstrap/compunaut_platform_shutdown.sh
+++ bootstrap/compunaut_platform_shutdown.sh
@@ -2,6 +2,8 @@
 ### FUNCTIONS
 cd "${0%/*}"
 
+salt -I 'compunaut_rundeck:enabled:True' cmd.run 'shutdown now'
+sleep 75
 salt -I 'compunaut_kvm:enabled:True' cmd.run "for vm in $(virsh list | awk '/compunaut/ {print $2}'); do virsh shutdown ${vm}; done"
 sleep 75
 salt -I 'compunaut_kvm:enabled:True' cmd.run 'shutdown now'
