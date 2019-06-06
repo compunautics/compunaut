bc2ff8757193066840117ac78ba961d8fdd6d5cc
Jesse Pendergrass <compunautics@gmail.com>
2019-06-06 12:43:27 -0500

    Configure the platform recover script to operate in the new framework

--- bootstrap/compunaut_platform_recover.sh
+++ bootstrap/compunaut_platform_recover.sh
@@ -4,13 +4,14 @@
 cd "${0%/*}"
 source ./compunaut_functions
 
+  echo_blue "Update Data"
   time salt-run state.orch orch.update_data --state-output=mixed # update data
 
   echo_blue "Recover LDAP"
-  salt -C 'I@openldap:slapd_services:*' state.apply compunaut_openvpn.deploy,compunaut_openldap --state_output=mixed
+  salt -C 'I@compunaut_openldap:enabled:True' state.apply compunaut_openldap --state_output=mixed
 
   echo_blue "Recover MySQL"
-  salt -C 'I@mysql:server:*' state.apply compunaut_mysql.galera --state_output=mixed
+  salt -C 'I@compunaut_mysql:enabled:True' state.apply compunaut_mysql.galera --state_output=mixed
 
   minion_wait
 
