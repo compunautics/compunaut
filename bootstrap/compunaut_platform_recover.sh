4172bba51951d1f3cab86ddfe5e192e5f7980c40
Jesse Pendergrass <compunautics@gmail.com>
2019-06-06 13:00:44 -0500

    Include a restart to dnsmasq here

--- bootstrap/compunaut_platform_recover.sh
+++ bootstrap/compunaut_platform_recover.sh
@@ -7,11 +7,14 @@ source ./compunaut_functions
   echo_blue "Update Data"
   time salt-run state.orch orch.update_data --state-output=mixed # update data
 
+  echo_blue "Restart dnsmasq"
+  time salt '*' cmd.run 'systemctl restart dnsmasq'
+
   echo_blue "Recover LDAP"
-  salt -C 'I@compunaut_openldap:enabled:True' state.apply compunaut_openldap --state_output=mixed
+  time salt -C 'I@compunaut_openldap:enabled:True' state.apply compunaut_openldap --state_output=mixed
 
   echo_blue "Recover MySQL"
-  salt -C 'I@compunaut_mysql:enabled:True' state.apply compunaut_mysql.galera --state_output=mixed
+  time salt -C 'I@compunaut_mysql:enabled:True' state.apply compunaut_mysql.galera --state_output=mixed
 
   minion_wait
 
