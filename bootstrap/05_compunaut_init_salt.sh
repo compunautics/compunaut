6c6a6be6c5276add722fd709fb53c83ffeb61559
Jesse Pendergrass <compunautics@gmail.com>
2019-06-07 15:22:25 -0500

    Make the log level quiet

--- bootstrap/05_compunaut_init_salt.sh
+++ bootstrap/05_compunaut_init_salt.sh
@@ -10,32 +10,32 @@ source ./compunaut_functions
 
 ### HYPERVISOR SETUP
   echo_red "SET UP HYPERVISORS"
-  time salt-run state.orch orch.update_data --state-output=mixed # update data
+  time salt-run state.orch orch.update_data --state-output=mixed --log-level=quiet # update data
   echo_blue "Install KVM, boot, and salt VMs"
-  time salt-run state.orch orch.create_and_salt_vms --state-output=mixed
+  time salt-run state.orch orch.create_and_salt_vms --state-output=mixed --log-level=quiet
 
   echo_green "Waiting 60 seconds"
   sleep 60
 
-  time salt-run state.orch orch.configure_minions --state-output=mixed
-  time salt-run state.orch orch.update_data --state-output=mixed # update data
+  time salt-run state.orch orch.configure_minions --state-output=mixed --log-level=quiet
+  time salt-run state.orch orch.update_data --state-output=mixed --log-level=quiet # update data
 
 ### DEPLOY COMPUNAUT
   minion_wait
 
   echo_red "SET UP DEFAULT ENVIRONMENT"
-  time salt-run state.orch orch.update_data --state-output=mixed # update data
+  time salt-run state.orch orch.update_data --state-output=mixed --log-level=quiet # update data
   echo_blue "Generate and deploy PKI"
-  time salt-run state.orch orch.generate_pki --state-output=mixed
+  time salt-run state.orch orch.generate_pki --state-output=mixed --log-level=quiet
   echo_blue "Install default environment, and apply iptables rules"
-  time salt-run state.orch orch.apply_default_env --state-output=mixed
+  time salt-run state.orch orch.apply_default_env --state-output=mixed --log-level=quiet
 
   minion_wait
 
   echo_red "DEPLOY COMPUNAUT"
   echo_blue "Install Keepalived, DNS, and Consul"
-  time salt-run state.orch orch.install_keepalived_dns_consul --state-output=mixed
-  time salt-run state.orch orch.update_data --state-output=mixed # update data
+  time salt-run state.orch orch.install_keepalived_dns_consul --state-output=mixed --log-level=quiet
+  time salt-run state.orch orch.update_data --state-output=mixed --log-level=quiet # update data
 
   echo_blue "Install Piserver"
   salt-run state.orch orch.install_piserver --async
@@ -45,7 +45,7 @@ source ./compunaut_functions
   salt-run state.orch orch.install_nfs --async
 
   echo_blue "Install MySQL"
-  time salt-run state.orch orch.install_mysql --state-output=mixed
+  time salt-run state.orch orch.install_mysql --state-output=mixed --log-level=quiet
   echo_blue "Install InfluxDB"
   salt-run state.orch orch.install_influxdb --async
 
@@ -63,7 +63,7 @@ source ./compunaut_functions
   echo_green "Install Gitlab"
   salt-run state.orch orch.install_gitlab --async
   echo_green "Install Guacamole"
-  time salt-run state.orch orch.install_guacamole --state-output=mixed
+  time salt-run state.orch orch.install_guacamole --state-output=mixed --log-level=quiet
 
 # FINAL SETUP
   echo_green "Waiting 180 seconds"
@@ -71,8 +71,8 @@ source ./compunaut_functions
   minion_wait
 
   echo_red "FINAL SETUP"
-  time salt-run state.orch orch.update_data --state-output=mixed # update data
-  time salt-run state.orch orch.highstate --state-output=mixed
+  time salt-run state.orch orch.update_data --state-output=mixed --log-level=quiet # update data
+  time salt-run state.orch orch.highstate --state-output=mixed --log-level=quiet
 
 # Don't exit until all salt minions are answering
   minion_wait
