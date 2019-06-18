2311b24b366b548fbffdd0323403e1139fa48e2a
Jesse Pendergrass <compunautics@gmail.com>
2019-06-18 01:45:45 -0500

    Modifying bootstrap scripts to work and be less verbose where not needed

--- bootstrap/compunaut_platform_recover.sh
+++ bootstrap/compunaut_platform_recover.sh
@@ -5,7 +5,7 @@ cd "${0%/*}"
 source ./compunaut_functions
 
   echo_blue "Update Data"
-  time salt-run state.orch orch.update_data --state-output=mixed # update data
+  time salt-run state.orch orch.update_data --state-output=mixed --log-level=quiet # update data
 
   echo_blue "Restart dnsmasq"
   time salt '*' cmd.run 'systemctl restart dnsmasq'
@@ -14,12 +14,12 @@ source ./compunaut_functions
   time salt '*' cmd.run 'systemctl restart chrony'
 
   echo_blue "Recover LDAP"
-  time salt -C 'I@compunaut_openldap:enabled:True' state.apply compunaut_openldap --state_output=mixed
+  time salt -C 'I@compunaut_openldap:enabled:True' state.apply compunaut_openldap --state_output=mixed --log-level=quiet
 
   echo_blue "Recover MySQL"
-  time salt -C 'I@compunaut_mysql:enabled:True' state.apply compunaut_mysql.galera --state_output=mixed
+  time salt -C 'I@compunaut_mysql:enabled:True' state.apply compunaut_mysql.galera --state_output=mixed --log-level=quiet
 
   minion_wait
 
-  time salt-run state.orch orch.highstate --state-output=mixed
+  time salt-run state.orch orch.highstate --state-output=mixed --log-level=quiet
 
