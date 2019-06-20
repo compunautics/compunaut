4ef71a599041ee752ef8f2109322974c213b74e4
Jesse Pendergrass <compunautics@gmail.com>
2019-06-20 16:27:32 -0500

    Updating the bootstrap directory

--- bootstrap/compunaut_vm_shutdown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for vm in $(virsh list | awk '/compunaut/ {print $2}'); do
-  virsh shutdown ${vm}
-done
--- bootstrap/compunaut_vm_startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for vm in $(virsh list --all | awk '/compunaut/ {print $2}'); do
-  virsh start ${vm}
-done
