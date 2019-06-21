4844611c5276da2e682fa13cf1ceed74d7c5dc8e
Jesse Pendergrass <compunautics@gmail.com>
2019-06-20 22:22:14 -0500

    Also grabbing job progress as a metric

--- salt/compunaut_telegraf/checks/check_octoprint.py
+++ salt/compunaut_telegraf/checks/check_octoprint.py
@@ -9,6 +9,7 @@ import socket
 hostname = socket.gethostname()
 headers = {"X-Api-Key":"{{ pillar.telegraf.secrets.octoprint_api_key }}","Content-Type":"application/json"}
 printer_url = "http://localhost:5000/api/printer"
+printer_job_url = "http://localhost:5000/api/job"
 printer_data = {}
 consul_agent_url = "http://localhost:8500/v1/agent/services"
 consul_health_url = "http://localhost:8500/v1/agent/health/service/name/"
@@ -19,10 +20,17 @@ try:
   # octoprint 
   r = requests.get(printer_url, headers=headers)
   if r.status_code is not 200:
-    print "Invalid response code from Octoprint."
+    print "Invalid response code from Octoprint when getting printer info."
     exit(2)
 
   printer_response = r.json()
+
+  r = requests.get(printer_job_url, headers=headers)
+  if r.status_code is not 200:
+    print "Invalid response code from Octoprint when getting job info."
+    exit(2)
+
+  printer_job_response = r.json()
 
   # consul
   r = requests.get(consul_agent_url)
@@ -37,7 +45,7 @@ except Exception as e:
 
 # parse responses
 try:
-  # octoprint
+  # octoprint printer status
   if printer_response['state']['flags']['printing'] == True:
     printer_data['printing'] = 1
   else:
@@ -52,6 +60,12 @@ try:
   printer_data['bed_target'] = printer_response['temperature']['bed']['target']
   printer_data['tool_actual'] = printer_response['temperature']['tool0']['actual']
   printer_data['tool_target'] = printer_response['temperature']['tool0']['target']
+
+  # octoprint job status
+  if type(printer_job_response['progress']['completion']) in (float, int) :
+    printer_data['progress'] = round(printer_job_response['progress']['completion'], 2)
+  else:
+    printer_data['progress'] = 'N/A'
 
   # consul
   for service, data in consul_response.iteritems():
@@ -83,5 +97,6 @@ print "octoprint,name=bed_actual temp="+str(printer_data['bed_actual'])
 print "octoprint,name=bed_target temp="+str(printer_data['bed_target'])
 print "octoprint,name=tool_actual temp="+str(printer_data['tool_actual'])
 print "octoprint,name=tool_target temp="+str(printer_data['tool_target'])
+print "octoprint,name=job_progress progress="+str(printer_data['progress'])
 print "octoprint,name=registered_filament,filament_name="+str(consul_data['filament_name'])+" filament=\""+str(consul_data['filament_name'])+"\",node=\""+hostname+"\""
 print "octoprint,name=filament_status,filament_name="+str(consul_data['filament_name'])+" status=\""+str(consul_data['filament_status'])+"\",node=\""+hostname+"\""
