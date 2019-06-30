90d532b885f08f5b24afac30d28d4f3faf336c8d
Jesse Pendergrass <compunautics@gmail.com>
2019-06-30 13:27:52 -0500

    Updating recovery steps
    We don't need to restart chronyd or dnsmasq anymore due to other fixes in the system.
    We do, however, need to restart haproxy. For some reason, certain things will return
    400 errors after a platform restart unless you do a restart here.

--- bootstrap/compunaut_platform_recover.sh
+++ bootstrap/compunaut_platform_recover.sh
@@ -7,17 +7,11 @@ source ./compunaut_functions
   echo_blue "Update Data"
   time salt-run state.orch orch.update_data --state-output=mixed --log-level=quiet # update data
 
-  echo_blue "Restart dnsmasq"
-  time salt '*' cmd.run 'systemctl restart dnsmasq'
-
-  echo_blue "Restart Chrony"
-  time salt '*' cmd.run 'systemctl restart chrony'
-
-  echo_blue "Recover LDAP"
-  time salt -C 'I@compunaut_openldap:enabled:True' state.apply compunaut_openldap --state_output=mixed --log-level=quiet
+  echo_blue "Restart Haproxy"
+  time salt -I 'compunaut_haproxy:enabled:True' cmd.run 'systemctl restart haproxy' 
 
   echo_blue "Recover MySQL"
-  time salt -C 'I@compunaut_mysql:enabled:True' state.apply compunaut_mysql.galera --state_output=mixed --log-level=quiet
+  time salt -I 'compunaut_mysql:enabled:True' state.apply compunaut_mysql.galera --state_output=mixed --log-level=quiet
 
   minion_wait
 
