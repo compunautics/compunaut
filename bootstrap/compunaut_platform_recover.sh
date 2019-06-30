f85d4aaa30109de241b50c2bbb58bd3c8b543428
Jesse Pendergrass <compunautics@gmail.com>
2019-06-30 18:56:40 -0500

    Restart rsyslog when doing the recovery
    For some reason, haproxy logs don't get properly forwarded after a hard down.
    Restarting rsyslog at recovery takes care of this.

--- bootstrap/compunaut_platform_recover.sh
+++ bootstrap/compunaut_platform_recover.sh
@@ -4,16 +4,20 @@
 cd "${0%/*}"
 source ./compunaut_functions
 
+  echo_blue "Restart Haproxy"
+  time salt -I 'compunaut_haproxy:enabled:True' cmd.run 'systemctl restart haproxy'
+
+  echo_blue "Restart Rsyslog"
+  time salt '*' cmd.run 'systemctl restart rsyslog'
+
   echo_blue "Update Data"
   time salt-run state.orch orch.update_data --state-output=mixed --log-level=quiet # update data
-
-  echo_blue "Restart Haproxy"
-  time salt -I 'compunaut_haproxy:enabled:True' cmd.run 'systemctl restart haproxy' 
 
   echo_blue "Recover MySQL"
   time salt -I 'compunaut_mysql:enabled:True' state.apply compunaut_mysql.galera --state_output=mixed --log-level=quiet
 
   minion_wait
 
+  echo_blue "Run Highstate"
   time salt-run state.orch orch.highstate --state-output=mixed --log-level=quiet
 
